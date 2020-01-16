7907ebd2eb685fa25465e1c6550682d0755defcb
Yosef <yegamble@users.noreply.github.com>
2020-01-17 08:41:34 +1300

    WP2Static CREATE wp-content/plugins/saasland-core/assets/js/appart-custom.js

---
+++ wp-content/plugins/saasland-core/assets/js/appart-custom.js
@@ -0,0 +1,2 @@
+;(function($){"use strict";$(document).ready(function(){function fAqactive(){$(".faq_accordian_two .card").on('click',function(){$(".faq_accordian_two .card").removeClass("active");$(this).addClass('active');});}
+fAqactive();});})(jQuery);
